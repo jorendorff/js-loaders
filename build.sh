bc74fca2e3456b217297bfb45d8aac90de6ee4cb
Jason Orendorff <jason.orendorff@gmail.com>
2013-10-12 09:03:30 -0500

    Temporary hack: keep old URLs alive for now.

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ rm -rf docs
 docco --layout classic Loader.js
 
 # Ship it.
+(cd docs && cp Loader.html loaders.html)  # legacy url
 (cd docs && rsync -avr --delete . people.mozilla.org:~/public_html/js-loaders/)
