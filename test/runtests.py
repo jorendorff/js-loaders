f2baee531f45b03326ad51a8a3b0721355038ad8
Jason Orendorff <jason.orendorff@gmail.com>
2013-10-25 05:21:30 -0700

    Include Promise.js from https://github.com/slightlyoff/Promises/tree/master/polyfill (just forked for now).

--- test/runtests.py
+++ test/runtests.py
@@ -4,7 +4,9 @@ sys.path.insert(0, PY_LIB_DIR)
 import jittests
 TEST_DIR = jittests.TEST_DIR = os.path.abspath(os.path.dirname(__file__))
 TEST_SCRIPT = os.path.abspath("../../mi/js/src/jit-test/jit_test.py")
-LOADER_JS = os.path.abspath(os.path.join(TEST_DIR, "..", "Loader.js"))
-sys.argv = [TEST_SCRIPT, "-s", "-o", "--args=-f '{}'".format(LOADER_JS), os.environ["JS"]]
+SRC_DIR = os.path.dirname(TEST_DIR)
+PROMISE_JS = os.path.join(SRC_DIR, "Promise.js")
+LOADER_JS = os.path.join(SRC_DIR, "Loader.js")
+sys.argv = [TEST_SCRIPT, "-s", "-o", "--args=-f '{}' -f '{}'".format(PROMISE_JS, LOADER_JS), os.environ["JS"]]
 execfile(TEST_SCRIPT)
     
